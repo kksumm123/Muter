a1a33f50c9e54a54079c652f71fa144a724f5354
kksumm123 <kksumm123@gmail.com>
2021-09-08 02:07:23 +0900

    1. Move 완성 2. Project Setting 중력 4배

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed = 15f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    #region Move
+    Vector3 move;
+    Vector3 relateMove;
+    void Move()
+    {
+        move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) move.z = 1;
+        if (Input.GetKey(KeyCode.S)) move.z = -1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+
+        if (move != Vector3.zero)
+        {
+            move.Normalize();
+
+            relateMove = Vector3.zero;
+            relateMove = transform.forward * move.z;
+            relateMove += transform.right * move.x;
+            relateMove.y = 0;
+            transform.Translate(speed * Time.deltaTime * relateMove);
+        }
+    }
+    #endregion Move
 }
