2842f72c367f941a8a9c66dfaf682001f8e375ac
kksumm123 <kksumm123@gmail.com>
2021-09-08 02:14:39 +0900

    1. Move 이동 Space Type 수정 2. 마우스 바라보도록 수정

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,12 +8,13 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 15f;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         Move();
+        LookAtMouse();
     }
 
     #region Move
@@ -36,8 +37,36 @@ public class Player : MonoBehaviour
             relateMove = transform.forward * move.z;
             relateMove += transform.right * move.x;
             relateMove.y = 0;
-            transform.Translate(speed * Time.deltaTime * relateMove);
+            transform.Translate(speed * Time.deltaTime * relateMove, Space.World);
         }
     }
     #endregion Move
+
+    #region LookAtMouse
+    Plane plane = new Plane(new Vector3(0, 1, 0), 0);
+    void LookAtMouse()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 dir = hitPoint - transform.position;
+            dir.y = 0;
+            dir.Normalize();
+            RotationSlerp(dir);
+        }
+    }
+    #endregion LookAtMouse
+
+    #region Methods
+    float fastAimingDistance = 0.2f;
+    float lookatRotationValue = 0.05f;
+    void RotationSlerp(Vector3 dir)
+    {
+        if (Vector3.Distance(transform.forward, dir) < fastAimingDistance)
+            transform.forward = Vector3.Slerp(transform.forward, dir, lookatRotationValue * 10);
+        else
+            transform.forward = Vector3.Slerp(transform.forward, dir, lookatRotationValue);
+    }
+    #endregion Methods
 }
