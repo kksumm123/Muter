cfecf7eeb3210814aa4d099fd715676214d734d2
kksumm123 <kksumm123@gmail.com>
2021-09-10 03:19:09 +0900

    1. map 교체 2. Q, E 카메라 상대적 회전 구현

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -6,13 +6,47 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     public Transform target;
-    Vector3 posisionOffset;
+    [SerializeField] Vector3 moveBackValue;
     void Start()
     {
-        posisionOffset = transform.position - target.position;
+        moveBackValue = new Vector3(0, 30, -25);
+        rotateDegree = 0;
     }
+
+
     void Update()
     {
-        transform.position = target.position + posisionOffset;
+        RelativeRotation();
+    }
+
+    [SerializeField] float rotateDegreeValue = 5;
+    float rotateDegree;
+    float forwardDegree;
+    float dirRadian;
+    Vector3 dir;
+    void RelativeRotation()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            rotateDegree += rotateDegreeValue;
+        if (Input.GetKey(KeyCode.E))
+            rotateDegree -= rotateDegreeValue;
+
+        forwardDegree = transform.forward.VectorToDegree();
+        dirRadian = (rotateDegree + 90) * Mathf.PI / 180;
+        dir.x = Mathf.Cos(dirRadian);
+        dir.z = Mathf.Sin(dirRadian);
+
+        transform.forward = dir;
+        transform.position = target.position;
+        transform.Translate(moveBackValue);
+        transform.LookAt(target);
+    }
+}
+static public class MyExtension
+{
+    public static float VectorToDegree(this Vector3 v)
+    {
+        float radian = Mathf.Atan2(v.z, v.x);
+        return (radian * Mathf.Rad2Deg);
     }
 }
