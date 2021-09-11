a77333ea42749ac77830c9f030a2d914a6b5b24f
kksumm123 <kksumm123@gmail.com>
2021-09-11 19:08:37 +0900

    1. Bullet.cs 초안 작성 완료 2. Bullet 트리거 체크

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    readonly string bulletCaseString = "BulletCase";
+    GameObject bulletCase;
+
+    [SerializeField] float speed = 40;
+    [SerializeField] float destroyTime = 7;
     void Start()
     {
-        
+        bulletCase = (GameObject)Resources.Load(bulletCaseString);
+        Destroy(gameObject, destroyTime);
     }
 
     void Update()
     {
-        
+        transform.Translate(speed * Time.deltaTime * transform.forward);
     }
 }
